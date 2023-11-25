74ba17e1002de670cd739072501988e6d1bfac79
Shirshak Shrestha <shirshak97@gmail.com>
2023-11-25 15:15:04 +0545

    security packages

--- src/app.js
+++ src/app.js
@@ -1,5 +1,37 @@
 const express = require("express");
+const compression = require("compression");
+
+// Security
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const cors = require("cors");
 
 const app = express();
+
+// set security HTTP headers
+app.use(helmet());
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(xss());
+app.use(mongoSanitize());
+
+// gzip compression
+app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
+// Test initial route
+app.get("/", (req, res) => {
+  res.send("Initial Page");
+});
 
 module.exports = app;
